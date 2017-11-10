445546f6c17f81ac988a7ad127b486c8e68eb65b
balvares <brunaalvares280@yahoo.com.br>
2017-11-09 23:32:13 -0200

    2017-11-09_DAO

--- Model/DAO/Especifico/ContaPagarDAO.cs
+++ Model/DAO/Especifico/ContaPagarDAO.cs
@@ -7,7 +7,8 @@ using System.Data.SqlClient;
 namespace Model.DAO.Especifico
 {
 	public class ContaPagarDAO
-	{
+    {
+        #region CONTAS A PAGAR
 
         #region Observações
 
@@ -43,7 +44,7 @@ namespace Model.DAO.Especifico
                 throw ex;
             }	
         }
-
+        
 		public List<ContaPagar> buscaPorData(DateTime dt1, DateTime dt2)
 		{
             query = null;
@@ -86,19 +87,74 @@ namespace Model.DAO.Especifico
             }
 
             return lstContaPagar;
-        }		
+        }
 
-		public List<ContaPagar> buscaPorFornecedor(Fornecedor fornecedor)
-		{
-	return	}		
+        public List<ContaPagar> buscaPorTipo(int tipo)
+        {
+            //A ideia no form seria primeiro uma mini busca dos tipos que ja existem, depois de preenchido essa string
+            //é preenchida com o id do tipo que a pessoa escolheu...
+            query = null;
+            List<ContaPagar> lstContaPagar = new List<ContaPagar>();
+            try
+            {
+                query = "SELECT TC.DESCRICAO, F.RAZAO_SOCIAL, CP.VALOR, CP.DT_PAGTO FROM CONTA_PAGAR AS CP "
+                        + "INNER JOIN TIPO_CONTA AS TC ON CP.ID_TIPO_CONTA = TC.ID_TIPO_CONTA "
+                        + "INNER JOIN FORNECEDOR AS F ON CP.ID_FORNECEDOR = F.ID_FORNECEDOR "
+                        + "WHERE CP.STS_ATIVO = 1 AND CP.TIPO = " + (tipo).ToString();
+                lstContaPagar.Add(setarObjeto(banco.MetodoSelect(query)));
+            }
 
-		public List<ContaPagar> buscaPorValor(string tipoConta)
-		{
-	return	}
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return lstContaPagar;
+        }
+
+        //VERIFICAR COM CARLOS!
+        //public List<ContaPagar> buscaPorNomeFornecedor(Fornecedor fornecedor)
+        //{
+        //    query = null;
+        //    List<ContaPagar> lstContaPagar = new List<ContaPagar>();
+        //    try
+        //    {
+        //        query = "SELECT TC.DESCRICAO, F.RAZAO_SOCIAL, CP.VALOR, CP.DT_PAGTO FROM CONTA_PAGAR AS CP "
+        //                + "INNER JOIN TIPO_CONTA AS TC ON CP.ID_TIPO_CONTA = TC.ID_TIPO_CONTA "
+        //                + "INNER JOIN FORNECEDOR AS F ON CP.ID_FORNECEDOR = F.ID_FORNECEDOR "
+        //                + "WHERE F.RAZAO_SOCIAL "
+        //                + "AND CP.STS_ATIVO = 1";
+        //        lstContaPagar.Add(setarObjeto(banco.MetodoSelect(query)));
+        //    }
+
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+
+        //    return lstContaPagar;
+        //}
 
 		public List<ContaPagar> listaContaPagar()
 		{
-	return	}
+            query = null;
+            List<ContaPagar> lstContaPagar = new List<ContaPagar>();
+            try
+            {
+                query = "SELECT TC.DESCRICAO, F.RAZAO_SOCIAL, CP.VALOR, CP.DT_PAGTO FROM CONTA_PAGAR AS CP "
+                        + "INNER JOIN TIPO_CONTA AS TC ON CP.ID_TIPO_CONTA = TC.ID_TIPO_CONTA "
+                        + "INNER JOIN FORNECEDOR AS F ON CP.ID_FORNECEDOR = F.ID_FORNECEDOR "
+                        + "WHERE CP.STS_ATIVO = 1";
+                lstContaPagar.Add(setarObjeto(banco.MetodoSelect(query)));
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return lstContaPagar;
+        }
 
         public bool altera(ContaPagar cp)
         {
@@ -163,6 +219,12 @@ namespace Model.DAO.Especifico
                         case "DT_PAGTO":
                             obj.data = Convert.ToDateTime(dr[idx]);
                             break;
+                        case "ID_FORNECEDOR":
+                            obj.fornecedor.id_fornecedor = Convert.ToInt32(dr[idx]);
+                            break;
+                        case "ID_TIPO_CONTA":
+                            obj.id_tipo = Convert.ToInt32(dr[idx]);
+                            break;
                     }
                 }
             }
@@ -174,6 +236,49 @@ namespace Model.DAO.Especifico
             }
 
             return obj;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region TIPO DE CONTA
+
+        public List<ContaPagar> buscaTipoPorDescricao(ContaPagar tipo)
+        {
+            query = null;
+            List<ContaPagar> lstTipos = new List<ContaPagar>();
+            try
+            {
+                query = "SELECT DESCRICAO FROM TIPO_CONTA WHERE DESCRICAO LIKE '%" + tipo.desc_conta + "%' "
+                        + " AND STS_ATIVO = 1";
+                lstTipos.Add(setarObjeto(banco.MetodoSelect(query)));
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return lstTipos;
+        }
+
+        public List<ContaPagar> buscaTipo()
+        {
+            query = null;
+            List<ContaPagar> lstTipos = new List<ContaPagar>();
+            try
+            {
+                query = "SELECT DESCRICAO FROM TIPO_CONTA WHERE STS_ATIVO = 1";
+                lstTipos.Add(setarObjeto(banco.MetodoSelect(query)));
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return lstTipos;
         }
 
         #endregion
